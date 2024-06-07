bec963e1935656ea64c517d26103b58f65ef9997
Edgar Joel Martín Melián <martinmelianedgarjoel@gmail.com>
2024-06-07 18:46:31 +0100

    Update Navbar.tsx

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -7,10 +7,25 @@ export function Navbar() {
       <nav>
         <ol>
           <li>
-            <a href="/p1_strings">Strings</a>
+            <a href='/p1_strings'>Strings</a>
           </li>
           <li>
-            <a href="/p2_lenguages">Lenguages</a>
+            <a href='/p2_lenguages'>Lenguages</a>
+          </li>
+          <li>
+            <a href='p3_calculator'>Calculator</a>
+          </li>
+          <li>
+            <a href='p4_code_analizer'>C++ Code Analyzer</a>
+          </li>
+          <li>
+            <a href='p5_autamata_simulator'>Automata Simulator</a>
+          </li>
+          <li>
+            <a href='p6_dfa2grammar'>DFA to Grammar</a>
+          </li>
+          <li>
+            <a href='p7_grammar2cnf'>Grammar to CNF</a>
           </li>
         </ol>
       </nav>
