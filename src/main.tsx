5d3dda3cc2e3b271bd1a726fa4f94dd1b71ca854
Edgar Joel Martín Melián <martinmelianedgarjoel@gmail.com>
2024-06-07 17:55:25 +0100

    Added root Router

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom"
+
 import './index.css'
+
+import Root from './routes/root'
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App/>
+    element: <Root/>
   },
 ])
 
---
+++ src/routes/root.tsx
@@ -0,0 +1,22 @@
+export default function Root() {
+  return (
+    <>
+      <aside>
+        <h1>Proyects</h1>
+        <nav>
+          <ol>
+            <li>
+              <a href="/p1_strings">Strings</a>
+            </li>
+            <li>
+              <a href="/p2_lenguages">Lenguages</a>
+            </li>
+          </ol>
+        </nav>
+      </aside>
+      <main>
+        <h1>Root</h1>
+      </main>
+    </>
+  )
+}
