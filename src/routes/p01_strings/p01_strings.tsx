98a210598b7111053f3d609e2e64b83adb0faa34
Edgar Joel Martín Melián <martinmelianedgarjoel@gmail.com>
2024-06-13 19:42:28 +0100

    Styles update
    Lots of changes

--- src/routes/p01_strings/p01_strings.tsx
+++ src/routes/p01_strings/p01_strings.tsx
@@ -10,16 +10,46 @@ const representation = {
   empty_chain: 'λ',
 }
 
+const spanish_alphabet:Symbol[] = [
+  {data: 'a'},
+  {data: 'b'},
+  {data: 'c'},
+  {data: 'd'},
+  {data: 'e'},
+  {data: 'f'},
+  {data: 'g'},
+  {data: 'h'},
+  {data: 'i'},
+  {data: 'j'},
+  {data: 'k'},
+  {data: 'l'},
+  {data: 'm'},
+  {data: 'n'},
+  {data: 'ñ'},
+  {data: 'o'},
+  {data: 'p'},
+  {data: 'q'},
+  {data: 'r'},
+  {data: 's'},
+  {data: 't'},
+  {data: 'u'},
+  {data: 'v'},
+  {data: 'w'},
+  {data: 'x'},
+  {data: 'y'},
+  {data: 'z'},
+]
+
 export function PStrings() {
 
-  const [alphabet, setAlphabet] = useState<Symbol[]>([])
+  const [alphabet, setAlphabet] = useState<Symbol[]>(spanish_alphabet)
   const [alphabetInputValue, setAlphabetInputValue] = useState('')
 
   const handleAddButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     event.preventDefault()
     const symbol_data = alphabetInputValue.trim()
     if (symbol_data.length > 0 && !alphabet.some((symbol) => symbol.data === symbol_data)) {
-      const new_symbol : Symbol = {data: symbol_data} 
+      const new_symbol: Symbol = {data: symbol_data} 
       setAlphabet([...alphabet, new_symbol])
     }
     setAlphabetInputValue('')
@@ -27,16 +57,10 @@ export function PStrings() {
 
   const [chain, setChain] = useState<Symbol[]>([])
 
-  const handleSymbolDragStart = (event: React.DragEvent<HTMLSpanElement>, symbol: Symbol) => {
-    event.dataTransfer.setData('text/plain', symbol.data)
+  const addSymbolToChain = (symbol:Symbol) => {
+    setChain([...chain, symbol])
   }
 
-  const handleChainDrop = (event: React.DragEvent<HTMLDivElement>) => {
-    event.preventDefault();
-    const symbol_data = event.dataTransfer.getData('text/plain');
-    const new_symbol: Symbol = {data: symbol_data};
-    setChain([...chain, new_symbol])
-  }
 
 
   return (
@@ -54,29 +78,27 @@ export function PStrings() {
             <img src='/icons/plus.svg' alt='Add symbol icon'></img> 
           </button>
         </form>
-        <div>
-          <div className='alphabet'>
-            <strong>
-              {`${representation.alphabet} =`}
-              <span>{`{`}</span>
-            </strong>
-            <div className='symbolContainer'>
-              {alphabet.map((symbol, index) => (
-                <span key={index} draggable onDragStart={(event) => handleSymbolDragStart(event, symbol)}>
+        <div className='alphabet'>
+          <label>
+            {`${representation.alphabet} =`}
+            <span>{`{`}</span>
+          </label>
+          <div className='symbolsContainer'>
+            {alphabet.map((symbol, index) => (
+              <div key={index} className='alphabetSymbol' onClick={(event) => {event.preventDefault(); addSymbolToChain(symbol)}}>
+                <p key={index}>
                   {`${symbol.data}`}
-                </span>
-              ))}
-            </div>
-            <strong><span>{`}`}</span></strong>
+                </p>
+              </div>
+            ))}
           </div>
+          <label>{`}`}</label>
         </div>
       </section>
       <section className='chain'>
         <h2>Chain</h2>
         <div 
-          className='draggableChain'
-          onDragOver={(event) => event.preventDefault()}
-          onDrop={handleChainDrop}
+          className='chainContainer'
         >
           {chain.length !== 0 ? 
             chain.map((symbol, index) => (
@@ -85,12 +107,11 @@ export function PStrings() {
               </span>
             ))
             :
-            (<p>
+            (<div className='emptyChainText'>
               <span>{`${representation.empty_chain}`}</span>
-              Drag Symbols from the Alphabet to the container to form a Chain
-            </p>)}
+              <p>{`(Click on the Symbols from the Alphabet to form a chain)`}</p>
+            </div>)}
         </div>
-
       </section>
     </div>
   )
